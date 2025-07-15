b78c550aeac9f6edfbe64ba33052f03202b189ec
OQdevpy <117621709+OQdevpy@users.noreply.github.com>
2025-07-15 20:31:21 +0500

    Update base.py

--- core/settings/base.py
+++ core/settings/base.py
@@ -17,7 +17,11 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
 CSRF_TRUSTED_ORIGINS = [
     "http://hisobot2.stat.uz",
+    "https://hisobot2.stat.uz",
 ]
+
+
+
 # Application definition
 DJANGO_APPS = [
     'jazzmin',
